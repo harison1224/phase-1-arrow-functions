90d1a21e411808807bba8544a253e69441507cd3
harison1224 <harisonochieng0@gmail.com>
2024-07-03 13:47:07 +0300

    competed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+//A function expression called divide that divides 2000 by 100..
+const divide = function(){
+    return 2000 / 100;
+};
+//An arrow function called square that takes one parameter and multiplies it by it's self..
+const square = (M) => M * M;
+//An arrow function called add that takes two parameters and adds them tongether...
+const add = (a,b) => a + b;
+let a =3
+let b =4 ;
